ab491ebb0e15f40e6a5bbd59016c03d4280ec44b
Westphal Raphael <westphal.raphael@gmail.com>
2019-01-17 10:48:18 +0100

    Better usage of parallel

--- download.sh
+++ download.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 set -e
 
-UPDATE_YOUTUBEDL=${UPDATE_YOUTUBEDL:-1}
+#UPDATE_YOUTUBEDL=${UPDATE_YOUTUBEDL:-1}
+UPDATE_YOUTUBEDL=0
 export CONFIG_FILE="./config.json"
 export LIBRARY_FOLDER="./test/"
+export ALLOW_OVERWRITE=0
+export TMP_FOLDER="/tmp"
 MAX_CONCURRENT_DOWNLOAD=3
 
 ACTUAL_PATH="$(pwd)"
 export ACTUAL_PATH
 
-export ALLOW_OVERWRITE=1
 
 _required_tool() {
 	if ! type "$1" >/dev/null 2>/dev/null; then
@@ -32,21 +34,19 @@ LIBRARY_FOLDER=$(jq -r .libraryFolder config.json)
 LIBRARY_FOLDER=$(readlink -f "$LIBRARY_FOLDER")
 [ -d "$LIBRARY_FOLDER" ] || _print_err_and_exit "Library folder does not exist"
 
-if [ "$UPDATE_YOUTUBEDL" = 1 ]; then
+if [ "$UPDATE_YOUTUBEDL" = 1 ] || [ ! -f "youtube-dl" ]; then
     echo "Updating youtube-dl"
     curl -L https://yt-dl.org/downloads/latest/youtube-dl -o ./youtube-dl
     chmod +x ./youtube-dl
 fi
 
-# TODO: check that the binary exists
+TMP_FOLDER=$(mktemp -d)
+mkdir -p  $TMP_FOLDER
 
-rm -rf tmp || true
-mkdir tmp || true
+todoSoundUrl=$(mktemp)
+todoOutputFolder=$(mktemp)
 
 # TODO: tmp -> $(mktemp) ?
-
-#Convert to absolute path to keep consistency
-
 i=0
 url=$(jq -r ".playlistToSync[$i].url" "$CONFIG_FILE")
 while [ "$url" != "null" ]; do
@@ -75,49 +75,26 @@ while [ "$url" != "null" ]; do
 
         echo "Processing $soundUrl"
 
-        uuid=$(base32 /dev/urandom | head -c 15)
-        mkdir -p "tmp/$uuid"
-        echo "uuid=\"$uuid\"" >> "tmp/$uuid/worker.sh"
-
         type=$(echo "$tracklist" | jq -r ".entries[$u].ie_key")
 
-        #Add the outputFolder to the configuration of the worker
-        echo "outputFolder=\"$outputFolder\"" >> "tmp/$uuid/worker.sh"
-
-
-        # Add the title
-        if [ "$type" = "Soundcloud" ]; then
-            echo "title=\$(\"$ACTUAL_PATH/youtube-dl\" -e \"$soundUrl\")" >> "tmp/$uuid/worker.sh"
-
-            echo "Processing $soundUrl"
-
-        elif [ "$type" = "Youtube" ]; then
-            title=$(echo "$tracklist" | jq -r ".entries[$u].title")
-            echo "title=\"$title\"" >> "tmp/$uuid/worker.sh"
-
-            echo "Processing $title"
-
-        else
-            echo "$type is not supported, skipping"
-            continue
-        fi
-
         #Add the correct soundUrl
-        if [ "$type" = "Youtube" ]; then
-            echo "URL=\"https://www.youtube.com/watch?v=$soundUrl\"" >> "tmp/$uuid/worker.sh"
-        elif [ "$type" = "Soundcloud" ]; then
-            echo "URL=\"$soundUrl\"" >> "tmp/$uuid/worker.sh"
+        if  echo "$tracklist" | grep "youtube" > /dev/null; then
+            url="https://www.youtube.com/watch?v=$soundUrl"
+        elif  echo "$tracklist" | grep "soundcloud" > /dev/null; then
+            url="$soundUrl"
         else
             echo "$type is not supported, skipping"
             continue
         fi
+
+        echo "$url" >> $todoSoundUrl
+        echo "$outputFolder" >> $todoOutputFolder
 
         #TODO: Add username/password for youtube/souncloud
         #--username \"$Playlist::config->{'username'}\" \
         #--password \"$Playlist::config->{'password'}\"  \
 
-        # Add the rest of the worker
-        cat worker.sh >> "tmp/$uuid/worker.sh"
+        #Get the next url
         u=$((u + 1))
         soundUrl=$(echo "$tracklist" | jq -r ".entries[$u].url")
     done
@@ -132,5 +109,9 @@ if [ "$MAX_CONCURRENT_DOWNLOAD" -gt "$(nproc)" ]; then
 fi
 
 echo "Launching downloads using $MAX_CONCURRENT_DOWNLOAD max concurrent downloads"
+parallel --link -a "$todoSoundUrl" -a "$todoOutputFolder" ./worker.sh "{1}" "{2}"
+#find tmp -type f -iname "worker.sh" | parallel -j "$MAX_CONCURRENT_DOWNLOAD" sh
 
-find tmp -type f -iname "worker.sh" | parallel -j "$MAX_CONCURRENT_DOWNLOAD" sh
+rm -rf "$TMP_FOLDER"
+rm -rf "$todoOutputFolder"
+rm -rf "$todoSoundUrl"
--- worker.sh
+++ worker.sh
@@ -1,42 +1,55 @@
+set -e 
+
+ if [ -z "$1" ] || [ -z "$2" ]; then
+     echo "Usage : worker.sh link outputFolder"
+     exit 1
+ fi
+
+soundUrl="$1"
+outputFolder="$2"
+
+uuid=$(base32 /dev/urandom | head -c 15)
+mkdir -p "$TMP_FOLDER/$uuid"
+cd "$TMP_FOLDER/$uuid"  || exit
+
 count=0
 MAX_COUNT=3
 
-while [ $count -lt $MAX_COUNT ];do
+#TODO: Get the title 
+title="$("$ACTUAL_PATH/youtube-dl" -e "$soundUrl" | sed "s/\"|'|\\|\///g")"
 
-    #Escape title
-    title="$(echo "$title" | sed -E "s/\"|'|\\|//g")"
+if [ -f "$LIBRARY_FOLDER/$outputFolder/$title.mp3" ] && [ "$ALLOW_OVERWRITE" = "0" ]; then
+    echo "Skipping $title"
+    exit 0
+fi
 
-    if [ -f "$LIBRARY_FOLDER/$outputFolder/$title.mp3" -a "$ALLOW_OVERWRITE" = "0" ]; then
-        echo "Skipping $title"
-        exit 0
-    fi
+while [ $count -lt $MAX_COUNT ]; do
 
-    echo "Downloading $title"
+    echo "Downloading $title $count/$MAX_COUNT"
 
-    "$ACTUAL_PATH"/youtube-dl  \
-        -o "$ACTUAL_PATH/tmp/$uuid/%(title)s.%(ext)s" \
+    if "$ACTUAL_PATH"/youtube-dl  \
+        -o "$TMP_FOLDER/$uuid/%(title)s.%(ext)s" \
         -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' \
         -x --audio-format mp3 --audio-quality 0 \
         --add-metadata \
         --metadata-from-title  "%(artist)s  -  %(title)s" \
-        --embed-thumbnail --add-metadata -c  -i "$URL"
-
-    if [ $? != 0 ]; then
-        echo "Failed to download $title, retrying"
-        count=$((count + 1))
-        sleep 10
-    else
+        --embed-thumbnail --add-metadata -c -i "$soundUrl" > /dev/null
+    then
         break
+    else
+        count=$((count + 1))
+        sleep 2
     fi
 done
 
 if [ "$count" = "$MAX_COUNT" ]; then
     echo "Failed to download $title"
-    echo "$URL" > "$ACTUAL_PATH/failed_downloads.log"
+    echo "$soundUrl" > "$ACTUAL_PATH/failed_downloads.log"
     exit 1
 fi
 
-toMove=$(find "$ACTUAL_PATH/tmp/$uuid/" -iname "*.mp3")
+#TODO: Change this
+toMove=$(find "$TMP_FOLDER/$uuid/" -iname "*.mp3")
 echo "Moving $title"
 mv "$toMove" "$LIBRARY_FOLDER/$outputFolder/$title.mp3"
 
